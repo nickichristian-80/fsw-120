4aef68149d8043aff00a01e816711d303fad0deb
nickichristian-80 <nicki.christian@bryanuniversity.edu>
2020-10-29 18:01:30 -0500

    commit

---
+++ w3/todo-app/src/tddata.js
@@ -0,0 +1,59 @@
+const tddata =[
+    {
+        "name": "Cheeta",
+        "show": "ThunderCats",
+        "catchPhrase": "I am Cheeta. In my heart, I know I'm the strongest of my kind.The pride of my people. I am unique in this universe. And I will *not be DEFEATED*!",
+        "imageName": "https://i.pinimg.com/236x/a3/ba/83/a3ba83c5535a1d8f80b8f22bf6fae443.jpg"
+    },
+    {
+        "name": "Skeletor",
+        "show": "Master of the Universe",
+        "catchPhrase": "Listen, I am not nice, I am not kind and I am NOT WONDERFUL!",
+        "imageName": "https://i.pinimg.com/236x/2a/4e/e7/2a4ee708ec62643d7aaf02cac877907a.jpg"
+    },
+    {
+        "name": "Maya",
+        "show": "Maya the Bee",
+        "catchPhrase": "It certainly is a great day to be alive.",
+        "imageName": ""
+    },
+    {
+        "name": "Vickie",
+        "show": "Vickie the Viking",
+        "catchPhrase": "AH-HA!",
+        "imageName": ""
+    },
+    {
+        "name": "Borrie Veldt",
+        "show": "Die Swart Kat",
+        "catchPhrase": "Die Swart Kat slann toe.",
+        "imageName": ""
+    },
+    {
+        "name": "Tin Tin",
+        "show": "The Calculus Affair",
+        "catchPhrase": "They're trying to stop us with that ramshackle erection!",
+        "imageName": ""
+    },
+    {
+        "name": "Obelix",
+        "show": "Asterix and Obelix",
+        "catchPhrase": "Its sont fous ces romains!",
+        "imageName": ""
+    },
+    {
+        "name": "Levinia",
+        "show": "Liewe Heksie",
+        "catchPhrase": "O Griet!",
+        "imageName": ""
+    },
+    {
+        "name": "Zummi",
+        "show": "Gummy Bears",
+        "catchPhrase": "As you all know, the Great Book of Gummi will never be finsihed",
+        "imageName": "https://i.pinimg.com/236x/ff/b4/52/ffb4529d23092fb6af3b1a9f4d25e318.jpg"
+    },
+
+]
+
+export default tddata
---
+++ w3/todo-app/src/todo-data.js
@@ -0,0 +1,11 @@
+const tododata = [
+    {
+        id: "Cheeta",
+        text: "Thunder Cats",
+        completed: true,
+        description: "I am a Cheeta",
+        img: "https://i.pinimg.com/236x/a3/ba/83/a3ba83c5535a1d8f80b8f22bf6fae443.jpg"
+    }
+]
+
+export default tododata
