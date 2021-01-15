a7876d7f8b947549e48d530332633f03e243286b
nickichristian-80 <nicki.christian@bryanuniversity.edu>
2021-01-14 23:12:15 -0600

    commit

---
+++ w7/Twitter/my-app/src/TweetBox.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+
+
+function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JV9nA0fR2J_MJLtmC3UrXAHaHa%26pid%3DApi&f=1" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
